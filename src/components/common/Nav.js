9124a7e869073c33689e26004e8aac51ba822d20
Gillesedgy <edgygilles@pursuit.org>
2022-11-01 11:20:18 -0400

    Router,Routes,Route Created

--- src/components/common/Nav.js
+++ src/components/common/Nav.js
@@ -1,18 +1,22 @@
+import { Link } from "react-router-dom";
 import "./Nav.css";
 
 function Nav() {
   return (
     <header>
       <article>
-        <h1>
-          <a href="/">
+        <Link to="/" className="HeaderLink">
+          <h1>
+            {/* <a href="/"> */}
             Northwest <span>Animal Hospital</span>
-          </a>
-        </h1>
+            {/* </a> */}
+          </h1>{" "}
+        </Link>
       </article>
       <aside>
         <ul>
           <li>
+
             <a href="/staff">All Staff</a>
           </li>
           <li>
